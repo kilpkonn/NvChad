b0b7b607cfbcf0a3d4057903b1c30cc2b11d1c45
Tavo Annus <tavo.annus@gmail.com>
2021-09-20 19:41:40 +0300

    Update feline conf

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -392,8 +392,10 @@ components.active[3][10] = {
 }
 
 require("feline").setup {
-   default_bg = colors.statusline_bg,
-   default_fg = colors.fg,
-   components = components,
+  colors = {
+    fg = colors.fg,
+    bg = colors.statusline_bg,
+  },
+  components = components,
 }
 
