83884cc6a205ec417ebc026d7b555c18d42927ff
Tavo Annus <tavo.annus@gmail.com>
2021-10-13 18:05:41 +0300

    Add new nvimtree cfg

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -16,40 +16,49 @@ g.nvim_tree_indent_markers = 1
 g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-
+--
 g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1
-    -- folder_arrows= 1
+   folders = 1,
+   -- folder_arrows= 1
+   files = 1,
+   git = git_status,
 }
+
 g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌"
-    },
-    folder = {
-        -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-        -- arrow_open = "",
-        -- arrow_closed = "",
-        default = "",
-        open = "",
-        empty = "", -- 
-        empty_open = "",
-        symlink = "",
-        symlink_open = ""
-    }
+   default = "",
+   symlink = "",
+   git = {
+      deleted = "",
+      ignored = "◌",
+      renamed = "➜",
+      staged = "✓",
+      unmerged = "",
+      unstaged = "✗",
+      untracked = "★",
+   },
+   folder = {
+      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
+      -- arrow_open = "",
+      -- arrow_closed = "",
+      default = "",
+      empty = "", -- 
+      empty_open = "",
+      open = "",
+      symlink = "",
+      symlink_open = "",
+   },
 }
 
 nvimtree.setup {
-   lsp_diagnostics = false,
+   diagnostics = {
+      enable = false,
+      icons = {
+         hint = "",
+         info = "",
+         warning = "",
+         error = "",
+      },
+   },
    disable_netrw = true,
    hijack_netrw = true,
    ignore_ft_on_setup = { "dashboard" },
@@ -59,7 +68,7 @@ nvimtree.setup {
    update_cwd = true,
    update_focused_file = {
       enable = true,
-      update_cwd = true,
+      update_cwd = false,
    },
    view = {
       allow_resize = true,
